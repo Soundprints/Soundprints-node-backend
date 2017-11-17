66a80dd01bbe5e9fb1253d53b928f702b7540837
Svit Zebec <svit.zebec@gmail.com>
2017-11-17 15:32:54 +0100

    Initial project structure commit

---
+++ routes/api/index.js
@@ -0,0 +1,19 @@
+var router = require('express').Router();
+
+// TODO: Add requirements for other routes e.g. router.use('/profiles', require('./profiles'))
+
+router.use(function(err, req, res, next){
+    if(err.name === 'ValidationError'){
+      return res.status(422).json({
+        errors: Object.keys(err.errors).reduce(function(errors, key){
+          errors[key] = err.errors[key].message;
+  
+          return errors;
+        }, {})
+      });
+    }
+  
+    return next(err);
+  });
+  
+  module.exports = router;
